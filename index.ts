e997d17ae17bf8fabee1ab2c06c70f6858d9f2b6
adeelikram <rahil.ikram67@gmail.com>
2021-11-03 18:27:06 +0500

    Hello

---
+++ index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+var app = express();
+app.set("view-engine", "ejs")
+app.use(express.static(__dirname + '/public'));
+app.get("/", (req, res) => {
+    res.render("index.ejs")
+})
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000")
+})
---
+++ public/js/google.js
@@ -0,0 +1,31 @@
+
+
+function init() {
+    gapi.load("auth2", async () => {
+        var auth = gapi.auth2.init({
+            client_id: "527034415010-hua0stku4spekrpneoae6kr1ej22t5cq.apps.googleusercontent.com",
+            scope: "https://www.googleapis.com/auth/contacts.readonly"
+        })
+        auth.attachClickHandler(document.getElementById("googleBtn"), {},
+            async (googleUser) => {
+                var token =await googleUser.getAuthResponse().access_token
+                var xml = new XMLHttpRequest()
+                var url = "https://www.google.com/m8/feeds/contacts/default/full?alt=json&access_token=" + token + "&max-results=500&v=3.0"
+                xml.open("GET",url,true)
+                xml.send()
+                xml.onload=()=>{
+                    var contacts = JSON.parse(xml.response).feed.entry
+                    var temp = []
+                    for (const iterator of contacts) {
+                        if("gd$email" in iterator){
+                            var email = iterator["gd$email"][0].address
+                            var name = (iterator["title"]["$t"])?iterator["title"]["$t"] : email
+                            temp.push({name,email})
+                            parseData(temp)
+                        } 
+                    }
+                }
+            })
+    })
+}
+
